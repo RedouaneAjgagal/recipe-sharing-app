23b159dcafd9c82e60de5d950a7eb902fda248db
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-04-26 15:19:36 +0200

    Add single profile route and its functionality

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Response, Request, RequestHandler } from "express"
 import Profile from "../models/profile"
-import { UnauthenticatedError } from "../errors"
+import { NotFoundError, UnauthenticatedError } from "../errors"
+import { StatusCodes } from "http-status-codes"
 
 interface CustomRequest extends Request {
     user?: {
@@ -16,17 +17,26 @@ const currentUser: RequestHandler = async (req: CustomRequest, res) => {
 
 const userProfile: RequestHandler = async (req: CustomRequest, res) => {
     const { id } = req.user!;
-    const userInfo = await Profile.findOne({ user: id }).populate({ path: 'user', select: 'name email'});
+    const userInfo = await Profile.findOne({ user: id }).populate({ path: 'user', select: 'name email' });
     if (!userInfo) {
         throw new UnauthenticatedError("Failed to authenticate");
     }
-    res.json(userInfo);
+    res.status(StatusCodes.OK).json(userInfo);
 }
 
+const singleProfile: RequestHandler = async (req, res) => {
+    const { userId } = req.params;
+    const profile = await Profile.findById(userId).populate({path: "user", select: "name -_id"});
+    if (!profile) {
+        throw new NotFoundError(`Sorry but there is no user with id ${userId}`);
+    }
+    res.status(StatusCodes.OK).json(profile);
+}
 
 
 export {
     CustomRequest,
     currentUser,
-    userProfile
+    userProfile,
+    singleProfile
 }
--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { currentUser, userProfile } from "../controllers/userController";
+import { currentUser, userProfile, singleProfile } from "../controllers/userController";
 import authenticateUser from "../middlewares/authentication";
 
 router.get('/current-user', authenticateUser, currentUser);
 router.get('/settings', authenticateUser, userProfile);
+router.get('/:userId', singleProfile);
 
 
 
