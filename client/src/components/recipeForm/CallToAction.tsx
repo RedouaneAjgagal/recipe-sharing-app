2799f09a811bdd7fb963f67be7e219578ee930e3
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-05-15 23:31:16 +0200

    setup call to action update

--- client/src/components/recipeForm/CallToAction.tsx
+++ client/src/components/recipeForm/CallToAction.tsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom"
 
-const CallToAction = () => {
+interface Props {
+  for: "newRecipe" | "updateRecipe"
+}
+
+const CallToAction = (props: React.PropsWithoutRef<Props>) => {
+  const navigate = useNavigate();
+  const cancelHandler = () => {
+    return navigate("..");
+  }
+
   return (
-    <div className='py-4'>
-        <button className='bg-amber-700 w-full text-white py-2 rounded font-medium tracking-wide'>Sumbit</button>
+    <div className={`py-4 grid gap-2 ${props.for === "newRecipe" ? "grid-cols-2" : "grid-cols-3 fixed bottom-0 left-0 w-full bg-white px-2"}`}>
+        {props.for === "updateRecipe" && <button onClick={cancelHandler} className='w-full text-slate-500 py-2 rounded font-medium tracking-wide' type="button">Cancel</button>}
+        <button className='bg-amber-700 col-span-2 w-full text-white py-2 rounded font-medium tracking-wide'>{props.for === "newRecipe" ? "Sumbit" : "Update"}</button>
     </div>
   )
 }
