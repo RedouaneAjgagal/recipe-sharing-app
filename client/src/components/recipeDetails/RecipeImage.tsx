8bb6708fb124942207b9e5bf1a65ca858d91c15a
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-06-05 04:18:32 +0200

    fix sliders not working properly on tailwindCSS

--- client/src/components/recipeDetails/RecipeImage.tsx
+++ client/src/components/recipeDetails/RecipeImage.tsx
@@ -6,19 +6,12 @@ interface Props {
 
 const RecipeImage = (props: React.PropsWithoutRef<Props>) => {
     const translateX = (props.index - props.currentSlide) * 100;
-
-    // let work;
-    // if (props.index === 0 && props.currentSlide === 1) {
-    //     work = `translate-x-[200%]`
-    // } else {
-    //     work = `translate-x-[${translateX}%]`
-    // }
+    console.log(translateX);
 
     return (
-        <div className={`translate-x-[${translateX}%] w-full max-w-full h-full absolute duration-500`}>
+        <div style={{ transform: `translateX(${translateX}%)` }} className="w-full max-w-full h-full absolute duration-500">
             <img src={props.src} alt="recipe" className="rounded w-full h-full object-cover" />
         </div>
-        // translateX={`translate-x-[${((index - curSlide) * 100)}%]`}
     )
 }
 
